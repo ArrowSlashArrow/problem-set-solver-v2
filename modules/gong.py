58ffa48323dd8a73c9d7b7ffecb7717e134890a6
ArrowSlashArrow <153234615+ArrowSlashArrow@users.noreply.github.com>
2025-03-22 19:06:55 -0600

    moved some things around + settings

---
+++ modules/gong.py
@@ -0,0 +1,13 @@
+# name: Test2
+# description: another test module for the module system
+# made by arrow on 2025/03/22
+# todo: tag system
+
+def solver():
+    # example
+    print("add two numbers program")
+    a = input("first number: ")
+    b = input("second number: ")
+    c = input("third number: ")
+
+    print(f"sum: {int(a) + int(b) + int(c)}")
