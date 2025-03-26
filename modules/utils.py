4c5bd60ebae788419240bb56e09e726c8905eaf7
ArrowSlashArrow <153234615+ArrowSlashArrow@users.noreply.github.com>
2025-03-26 02:19:01 -0600

    more docs and finishing up v0.2

--- modules/utils.py
+++ modules/utils.py
@@ -1,5 +1,12 @@
 # utility file for modules
 # ignore_module
 
-def solver():
-    print("your mum")
+# parse num util function
+def parse_num(num: str):
+    try:
+        return int(num)
+    except ValueError:
+        try:
+            return float(num)
+        except ValueError:
+            return num
